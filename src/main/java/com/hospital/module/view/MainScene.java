d2640a3e05988835466bc8eb533f5d376f5f0151
Niyat <niyathannibal@gmail.com>
2024-05-22 12:48:58 +0300

    add styling and switching scene from mainscene to doctorscene

--- src/main/java/com/hospital/module/view/MainScene.java
+++ src/main/java/com/hospital/module/view/MainScene.java
@@ -1,7 +1,14 @@
 package com.hospital.module.view;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class MainScene {
 
@@ -21,9 +28,23 @@ public class MainScene {
     }
 
     @FXML
-    private void onDoctorClicked() {
+    private void onDoctorClicked(javafx.event.ActionEvent event) {
         // Add action for doctor button click
         System.out.println("Doctor button clicked");
+        try {
+            // Load the DoctorDashboardScene FXML
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("DoctorDashboardScene.fxml"));
+            Parent doctorDashboardRoot = loader.load();
+            
+            // Get the current stage
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            
+            // Set the new scene
+            stage.setScene(new Scene(doctorDashboardRoot));
+            stage.setTitle("Doctor Dashboard");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @FXML
